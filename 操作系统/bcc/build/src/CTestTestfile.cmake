03a296d4e5197352cffb131884d161d597a75988
Chinesexilinyu <112465223+Chinesexilinyu@users.noreply.github.com>
2022-12-13 11:09:27 +0800

    Add files via upload

---
+++ 操作系统/bcc/build/src/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/seed/Desktop/bcc/src
+# Build directory: /home/seed/Desktop/bcc/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("cc")
+subdirs("python")
+subdirs("lua")
