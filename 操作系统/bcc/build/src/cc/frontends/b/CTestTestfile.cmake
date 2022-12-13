fbbff075d3cbacdabd428964fdf1cd9f8ae8a2cd
Chinesexilinyu <112465223+Chinesexilinyu@users.noreply.github.com>
2022-12-13 11:13:33 +0800

    Add files via upload

---
+++ 操作系统/bcc/build/src/cc/frontends/b/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/seed/Desktop/bcc/src/cc/frontends/b
+# Build directory: /home/seed/Desktop/bcc/build/src/cc/frontends/b
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
