fb54d981a4f13286e39d1f5cd843713b4af4b94b
Chinesexilinyu <112465223+Chinesexilinyu@users.noreply.github.com>
2022-12-13 15:11:46 +0800

    Add files via upload

---
+++ 操作系统/bcc/build/man/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/seed/Desktop/bcc/man
+# Build directory: /home/seed/Desktop/bcc/build/man
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("man8")
