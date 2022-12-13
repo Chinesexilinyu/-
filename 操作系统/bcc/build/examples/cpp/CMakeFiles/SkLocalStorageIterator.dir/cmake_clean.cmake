b2babd9bb10286051b0a5c3a0f6b51ba61338449
Chinesexilinyu <112465223+Chinesexilinyu@users.noreply.github.com>
2022-12-13 15:10:55 +0800

    Add files via upload

---
+++ 操作系统/bcc/build/examples/cpp/CMakeFiles/SkLocalStorageIterator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SkLocalStorageIterator.dir/SkLocalStorageIterator.cc.o"
+  "SkLocalStorageIterator"
+  "SkLocalStorageIterator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SkLocalStorageIterator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
