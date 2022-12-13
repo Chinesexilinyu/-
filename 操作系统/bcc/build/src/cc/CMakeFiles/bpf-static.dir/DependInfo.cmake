f863693c78c99ccbb9d72c3f3ed1d6ad4b77e8a5
Chinesexilinyu <112465223+Chinesexilinyu@users.noreply.github.com>
2022-12-13 11:19:13 +0800

    Add files via upload

---
+++ 操作系统/bcc/build/src/cc/CMakeFiles/bpf-static.dir/DependInfo.cmake
@@ -0,0 +1,85 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/seed/Desktop/bcc/src/cc/bcc_elf.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/bcc_elf.c.o"
+  "/home/seed/Desktop/bcc/src/cc/bcc_perf_map.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/bcc_perf_map.c.o"
+  "/home/seed/Desktop/bcc/src/cc/bcc_proc.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/bcc_proc.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/bpf.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/bpf.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/bpf_prog_linfo.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/bpf_prog_linfo.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/btf.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/btf.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/btf_dump.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/btf_dump.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/gen_loader.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/gen_loader.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/hashmap.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/hashmap.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/libbpf.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/libbpf.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/libbpf_errno.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/libbpf_errno.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/libbpf_probes.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/libbpf_probes.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/linker.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/linker.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/netlink.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/netlink.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/nlattr.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/nlattr.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/relo_core.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/relo_core.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/ringbuf.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/ringbuf.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/str_error.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/str_error.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/strset.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/strset.c.o"
+  "/home/seed/Desktop/bcc/src/cc/libbpf/src/xsk.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/libbpf/src/xsk.c.o"
+  "/home/seed/Desktop/bcc/src/cc/perf_reader.c" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/perf_reader.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "EXPORT_USDT"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/lib/llvm-6.0/include/../tools/clang/include"
+  "../src"
+  "src"
+  "src/cc"
+  "../src/cc"
+  "src/cc/frontends/b"
+  "../src/cc/frontends/b"
+  "../src/cc/frontends/clang"
+  "/usr/lib/llvm-6.0/include"
+  "../src/cc/libbpf/include"
+  "../src/cc/libbpf/include/uapi"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/seed/Desktop/bcc/src/cc/bcc_syms.cc" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/bcc_syms.cc.o"
+  "/home/seed/Desktop/bcc/src/cc/common.cc" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/common.cc.o"
+  "/home/seed/Desktop/bcc/src/cc/usdt/usdt.cc" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/usdt/usdt.cc.o"
+  "/home/seed/Desktop/bcc/src/cc/usdt/usdt_args.cc" "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/usdt/usdt_args.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EXPORT_USDT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/llvm-6.0/include/../tools/clang/include"
+  "../src"
+  "src"
+  "src/cc"
+  "../src/cc"
+  "src/cc/frontends/b"
+  "../src/cc/frontends/b"
+  "../src/cc/frontends/clang"
+  "/usr/lib/llvm-6.0/include"
+  "../src/cc/libbpf/include"
+  "../src/cc/libbpf/include/uapi"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
