732f0b1f8bf691452f6c5febea90a1d96cb5cbdd
Chinesexilinyu <112465223+Chinesexilinyu@users.noreply.github.com>
2022-12-13 14:58:51 +0800

    Add files via upload

---
+++ 操作系统/bcc/build/examples/cpp/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/seed/Desktop/bcc/examples/cpp
+# Build directory: /home/seed/Desktop/bcc/build/examples/cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("pyperf")
