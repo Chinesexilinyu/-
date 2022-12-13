82e99c3c7a63d89dfdddf48c8759f1fe5de9a46d
Chinesexilinyu <112465223+Chinesexilinyu@users.noreply.github.com>
2022-12-13 11:06:26 +0800

    Add files via upload

---
+++ 操作系统/bcc/build/introspection/CMakeFiles/bps.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/seed/Desktop/bcc/introspection/bps.c" "/home/seed/Desktop/bcc/build/introspection/CMakeFiles/bps.dir/bps.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/lib/llvm-6.0/include/../tools/clang/include"
+  "../src/cc"
+  "../src/cc/api"
+  "../src/cc/libbpf/include/uapi"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/seed/Desktop/bcc/build/src/cc/CMakeFiles/bpf-static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
