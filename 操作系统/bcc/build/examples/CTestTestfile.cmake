d738eeed16ab7c547adca60965226e7fb4208369
Chinesexilinyu <112465223+Chinesexilinyu@users.noreply.github.com>
2022-12-13 11:23:19 +0800

    Add files via upload

---
+++ 操作系统/bcc/build/examples/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/seed/Desktop/bcc/examples
+# Build directory: /home/seed/Desktop/bcc/build/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("cpp")
+subdirs("lua")
+subdirs("networking")
+subdirs("tracing")
---
+++ 操作系统/bcc/build/examples/tracing/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/seed/Desktop/bcc/examples/tracing
+# Build directory: /home/seed/Desktop/bcc/build/examples/tracing
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
