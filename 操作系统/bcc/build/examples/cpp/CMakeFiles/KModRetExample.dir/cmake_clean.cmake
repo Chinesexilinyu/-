6556fc65590881491d8e5f5dff062cfd4263e7e8
Chinesexilinyu <112465223+Chinesexilinyu@users.noreply.github.com>
2022-12-13 15:09:42 +0800

    Add files via upload

---
+++ 操作系统/bcc/build/examples/cpp/CMakeFiles/KModRetExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KModRetExample.dir/KModRetExample.cc.o"
+  "KModRetExample"
+  "KModRetExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KModRetExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
